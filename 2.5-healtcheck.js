e0d1e31c947ca481cf539baaaf846300caf44774
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-12-15 16:33:18 +0200

    Changed request module to axios

--- 2.5-healtcheck.js
+++ 2.5-healtcheck.js
@@ -1,33 +1,31 @@
 const dotenv = require('dotenv');
 const express = require('express');
-const request = require('request');
+const axios = require('axios');
 const https = require('https');
+
 
 dotenv.config();
 
 const app = express();
 
 app.get('/random', function (req, res) {
-    request(
-        'https://yesno.wtf/api',
-        (err, response, body) => {
-            if (err) {
-                res.status(500).send({message: err});
-            }
-            const yesnoResponse = JSON.parse(body)
-            if (yesnoResponse.answer === 'yes'){
-                res.send({'result': true});
-            }else if (yesnoResponse.answer === 'no'){            
-                res.send({'result': false});
-            }else{
-                res.status(500).send({'result': 'maybe'});
-            }
+    axios.get('https://yesno.wtf/api')
+    .then((response) => {
+        if (response.data.answer === 'yes') {
+            res.send({'result': true});
+        }else if (response.data.answer === 'no') {
+            res.send({'result': false})
+        }else{
+            res.status(500).send({'result': 'maybe'});
         }
-    )
-});
+    })
+    .catch((error) => {
+        res.status(error.response.status).end();
+    })
+})
 
 app.get('/healthz', function (req, res) {
-    const healthzReq = https.request('https://yesno.wtf', {method: 'HEAD'}, (result) => {
+    https.request('https://yesno.wtf', {method: 'HEAD'}, (result) => {
         res.status(result.statusCode).end();
     }).on('error', (err) => {
         res.status(500).end();
