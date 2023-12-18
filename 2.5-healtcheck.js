161ae199562e7577be314563b24ad5198955743c
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-12-18 14:16:41 +0200

    Changed promises in to async/await

--- 2.5-healtcheck.js
+++ 2.5-healtcheck.js
@@ -1,36 +1,37 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const axios = require('axios');
-const https = require('https');
-
 
 dotenv.config();
 
 const app = express();
 
-app.get('/random', function (req, res) {
-    axios.get('https://yesno.wtf/api')
-    .then((response) => {
-        if (response.data.answer === 'yes') {
-            res.send({'result': true});
-        }else if (response.data.answer === 'no') {
-            res.send({'result': false})
-        }else{
-            res.status(500).send({'result': 'maybe'});
+app.get('/random', async function (req, res) {
+        try {
+            const response = await axios.get('https://yesno.wtf/api');
+            if (response.data.answer === 'yes') {
+                res.send({'result': true});
+            }else if (response.data.answer === 'no') {
+                res.send({'result': false})
+            }else{
+                res.status(500).send({'result': 'maybe'});
+            }
+        } catch (error) {
+            res.status(error.response.status).end();
         }
-    })
-    .catch((error) => {
-        res.status(error.response.status).end();
-    })
 })
 
-app.get('/healthz', function (req, res) {
-    https.request('https://yesno.wtf', {method: 'HEAD'}, (result) => {
-        res.status(result.statusCode).end();
-    }).on('error', (err) => {
+app.get('/healthz', async function (req, res) {
+    try {
+        const response = await axios({
+            method: 'HEAD',
+            url: 'https://yesno.wtf'
+        })
+        res.send(response.data)
+    }catch (error) {
         res.status(500).end();
-    }).end()
-})
+    }
+}) 
 
 app.listen(process.env.API_PORT, () => {
     console.log(`Server is running on ${process.env.API_PORT} port`)
